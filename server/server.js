ccfbbc87e93437908e292eed28ea594f2d519d0e
cauldyclark15 <jcing.jc@gmail.com>
2017-03-23 16:57:43 +0700

    Add post user login route

--- server/server.js
+++ server/server.js
@@ -121,6 +121,23 @@ app.post('/users', (req, res) => {
   });
 });
 
+// ------ users login
+
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then(user => {
+      return user.generateAuthToken()
+        .then(token => {
+          res.header('x-auth', token).send({ user });
+        })
+    })
+    .catch(err => {
+      res.status(400).send();
+    })
+})
+
 // ------- require auth middleware
 
 app.get('/users/me', authenticate, (req, res) => {
